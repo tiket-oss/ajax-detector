765220744eef04b6d54aba318c5b22fc3e22ebc7
Hawari Rahman <hawari.rahman17@gmail.com>
2019-07-18 13:35:09 +0700

    Allow page URL and file to be passed as flag

--- network.go
+++ network.go
@@ -3,7 +3,35 @@ package main
 import (
 	"context"
 	"io"
+	"log"
+
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/chromedp"
 )
 
 func MonitorPageNetwork(ctx context.Context, writer io.Writer, pageURL string) {
+
+	chromedp.ListenTarget(ctx, func(v interface{}) {
+		if event, ok := v.(*network.EventRequestWillBeSent); ok {
+			requestInfo, err := event.Request.MarshalJSON()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			newline := "\n"
+			requestInfo = append(requestInfo, newline...)
+			if _, err := writer.Write(requestInfo); err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
+
+	// navigate to a page, wait for an element, click
+	err := chromedp.Run(ctx,
+		network.Enable(),
+		chromedp.Navigate(pageURL),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
